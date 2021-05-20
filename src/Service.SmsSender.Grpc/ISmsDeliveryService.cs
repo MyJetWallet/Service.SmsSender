deae592e0cd724e5ab722e8120c7b552478b6836
Andrei <aversilov@gmail.com>
2021-05-20 14:39:54 +0300

    ISmsDeliveryService fixed

--- src/Service.SmsSender.Grpc/ISmsDeliveryService.cs
+++ src/Service.SmsSender.Grpc/ISmsDeliveryService.cs
@@ -1,11 +1,14 @@
-﻿using System.Threading.Tasks;
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
 using Service.SmsSender.Grpc.Models.Requests;
 using Service.SmsSender.Grpc.Models.Responses;
 
 namespace Service.SmsSender.Grpc
 {
+    [ServiceContract]
     public interface ISmsDeliveryService
     {
+        [OperationContract]
         Task<SendSmsResponse> SendSmsAsync(SendSmsRequest request);
     }
 }
