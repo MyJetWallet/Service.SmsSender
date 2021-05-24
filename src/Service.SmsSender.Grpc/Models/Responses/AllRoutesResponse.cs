fdf1f8fe15b678dca31c5b5cd80c62be9d939148
Andrei <aversilov@gmail.com>
2021-05-24 15:17:22 +0300

    DataContract missing in AllRoutesResponse bug fixed

--- src/Service.SmsSender.Grpc/Models/Responses/AllRoutesResponse.cs
+++ src/Service.SmsSender.Grpc/Models/Responses/AllRoutesResponse.cs
@@ -3,6 +3,7 @@ using Service.SmsSender.Domain.Models;
 
 namespace Service.SmsSender.Grpc.Models.Responses
 {
+    [DataContract]
     public class AllRoutesResponse
     {
         [DataMember(Order = 1)]
